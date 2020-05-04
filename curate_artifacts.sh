3f8c7f5698dc2602031a08b5206e46920c08734f
Derrick Gibelyou <derrickgw@users.noreply.github.com>
2020-05-04 09:42:03 -0600

    Remove code for Vivado 2013.4 kernel

--- curate_artifacts.sh
+++ curate_artifacts.sh
@@ -8,11 +8,7 @@ if [ -z "$KDIR" ]; then
   false
 fi
 
-if [ $VIVADO == '2013.4' ]; then
-  MDST=usr/lib/modules
-else
-  MDST=lib/modules
-fi
+MDST=lib/modules
 KREL=$(cat $KDIR/include/config/kernel.release)
 VERSION=$(cat $KDIR/include/config/kernel.release | grep -o -P '\d+\.\d+')
 BUILD_NUMBER=${BUILD_NUMBER:-0}
@@ -29,43 +25,29 @@ rsync -a $KDIR/usr/lib/modules/$KREL/kernel          build/$VIVADO/$MDST/$KREL/
 rsync -a $KDIR/usr/lib/modules/$KREL/modules.order   build/$VIVADO/$MDST/$KREL/
 rsync -a $KDIR/usr/lib/modules/$KREL/modules.builtin build/$VIVADO/$MDST/$KREL/
 
-if [ $VIVADO != '2013.4' ]; then
-  /sbin/depmod -a -b build/$VIVADO $KREL
-fi
+/sbin/depmod -a -b build/$VIVADO $KREL
 
 sed "s/VERSION=.*/VERSION=$KREL/" post_install_template.sh > build/post_install.sh
 
 cd build
 
-if [ $VIVADO == '2013.4' ]; then
-  mkdir -p drivers/2013.4
-  cp ../*/*.ko drivers/2013.4
-  cp ../*/*rules drivers
-  tar -czf armhf_drivers.tgz -C drivers .
-  cat ../installer.sh armhf_drivers.tgz > armhf_drivers_installer.sh
-  chmod +x armhf_drivers_installer.sh
+mkdir -p $VIVADO/etc/modules-load.d/
+echo sarspi > $VIVADO/etc/modules-load.d/sarspi.conf
+echo newhaven_lcd > $VIVADO/etc/modules-load.d/lcd.conf
+echo xilinx_jtag > $VIVADO/etc/modules-load.d/jtag.conf
+fpm --post-install post_install.sh  \
+  --output-type deb \
+  --description 'Linux kernel and modules for a Zynq based Nanosar C system' \
+  --license 'GPL' \
+  -m 'IMSAR FPGA Team <fpga@imsar.com>' \
+  --vendor 'IMSAR LLC' \
+  --url 'https://www.imsar.com/' \
+  --name grizzly-kernel \
+  -C $VIVADO \
+  --architecture armhf \
+  --version $VERSION \
+  --iteration $BUILD_NUMBER \
+  --force  \
+  --input-type dir .
 
-  cp post_install.sh $VIVADO/usr/lib/modules/
-  tar -czf kernel_modules_$KREL.tgz -C $VIVADO/ .
-else
-  mkdir -p $VIVADO/etc/modules-load.d/
-  echo sarspi > $VIVADO/etc/modules-load.d/sarspi.conf
-  echo newhaven_lcd > $VIVADO/etc/modules-load.d/lcd.conf
-  echo xilinx_jtag > $VIVADO/etc/modules-load.d/jtag.conf
-  fpm --post-install post_install.sh  \
-    --output-type deb \
-    --description 'Linux kernel and modules for a Zynq based Nanosar C system' \
-    --license 'GPL' \
-    -m 'IMSAR FPGA Team <fpga@imsar.com>' \
-    --vendor 'IMSAR LLC' \
-    --url 'https://www.imsar.com/' \
-    --name grizzly-kernel \
-    -C $VIVADO \
-    --architecture armhf \
-    --version $VERSION \
-    --iteration $BUILD_NUMBER \
-    --force  \
-    --input-type dir .
-
-  ln grizzly-kernel_${VERSION}-${BUILD_NUMBER}_armhf.deb grizzly-kernel_${VERSION}-latest_armhf.deb
-fi
+ln grizzly-kernel_${VERSION}-${BUILD_NUMBER}_armhf.deb grizzly-kernel_${VERSION}-latest_armhf.deb
