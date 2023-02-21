d243edea1d2ffa4a92b8cac4e396f844a91a3ecf
Brendon Jensen <brendon.jensen@imsar.com>
2023-02-21 13:59:14 -0700

    Install modules too
    Billed to NanoSAR D

--- install.sh
+++ install.sh
@@ -6,19 +6,22 @@ HOSTNAME=${2:?hostname required}
 SRC_KERNEL=sources/kernel/kernel-5.10
 BOOT_DST=/boot/imsar/
 
+KERNEL_VERSION=5.10.104-tegra
+DRIVERS_DIR=/usr/lib/modules/${KERNEL_VERSION}/kernel/drivers
+
 case $OP in
     "kernel")
+        # Image and Device Tree
         SRC_BOOT=$SRC_KERNEL/arch/arm64/boot
-
         SRC_IMAGE=$SRC_BOOT/Image
         SRC_DTB=$SRC_BOOT/dts/nvidia/tegra194-p2888-0001-p2822-0000.dtb
-
         scp $SRC_IMAGE $SRC_DTB $HOSTNAME:$BOOT_DST
+
+        # Kernel modules
+        rsync -a --progress --stats sources/modules/lib $HOSTNAME:/
         ;;
 
     "drivers")
-        kernel_version=5.10.104-tegra
-        DRIVERS_DIR=/usr/lib/modules/${kernel_version}/kernel/drivers
         IMSAR_DIR=${DRIVERS_DIR}/imsar/
 
         moduleFiles=$(find drivers -name '*.ko')
